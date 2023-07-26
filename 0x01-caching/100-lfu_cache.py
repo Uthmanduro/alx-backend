9911e73ea5c9cd329d2396eccfc7222c4a544ea2
Uthman Durosinlohun <udurosinlohun@gmail.com>
2023-07-25 19:54:41 -0700

    compulsory task done

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""implements the LFUCache class"""
+from base_caching import BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """inherits from base caching and is a caching system"""
+    def __init__(self):
+        """initialize the fifocache instances"""
+        super().__init__()
+        self.lfu = {}
+
+    def put(self, key, item):
+        """assign to the dict self.cache_data the item value for the key"""
+        if key is None or item is None:
+            return
+        keys = list(self.cache_data.keys())
+        if key in keys:
+            self.cache_data.pop(key)
+        self.cache_data.update({key: item})
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            key = list(self.cache_data.keys())[0]
+            self.cache_data.pop(key)
+            print("DISCARD:", key)
+
+    def get(self, key):
+        """return the value in self.cache_data linked to the key"""
+        val = self.cache_data.get(key)
+        if val is None:
+            return
+        self.cache_data.pop(key)
+        self.cache_data.update({key: val})
+        return self.cache_data.get(key, None)

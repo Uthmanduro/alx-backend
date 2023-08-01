14779b10ef65b94f6ccb32eb9dbbfc5ceb0f48b4
uthmanduro <udurosinlohun@gmail.com>
2023-08-02 00:24:55 +0100

    fixed

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -27,8 +27,8 @@ def get_locale():
 @app.route('/')
 def index():
     """ index route"""
-    home_title = _('Welcome to Holberton')
-    home_header = _('Hello world!')
+    home_title = _('home_title')
+    home_header = _('home_header')
     return render_template('3-index.html', home_title=home_title,
                            home_header=home_header)
 
